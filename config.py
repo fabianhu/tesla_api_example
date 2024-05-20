4bd37936f64801946debee5bdd8034dfcf30510e
fabianhu <developer@huslik.net>
2024-05-20 23:46:35 +0200

    update

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
-# example config file for Tesla API access
+# this is the configuration file.
+# an example file "config.py.example" is checked in with the project.
 
-tesla_vin = 'LRWYAAAAAAA135456' # your car's VIN - this library only supports one car at the moment
+home = (50.964668, 7.413264)
 
+tesla_vin = 'LRWYAAAAAAA135456'
 tesla_client_id = "aaaaaaaaaaaa-bbbb-cccc-ddddddddddd"
 tesla_client_secret = "ta-secret.aaaaaaaaaaaaaaaa"  # only needed during registration, so does not ever need to be on the Pi!
-
-tesla_redirect_domain = "your.domain"  # NO https://, just your domain.tld !!!
-
-# Location of your answer page for customer registration:
+# put your key pair here:
+#lib/tesla_api/TeslaKeys/privatekey.pem
+#lib/tesla_api/TeslaKeys/publickey.pem
+# and store the pubkey at: https://your.domain/.well-known/appspecific/com.tesla.3p.public-key.pem
+tesla_redirect_domain = "your.domain"  # NO https:// !!!
 tesla_redirect_uri = "https://your.domain/and/stuff/"  # start with https:// and include a tailing '/'!
-
 tesla_audience = "fleet-api.prd.eu.vn.cloud.tesla.com" # Europe
 #tesla_audience =  "fleet-api.prd.na.vn.cloud.tesla.com" # North America
-
 tesla_scopes = "user_data vehicle_device_data vehicle_cmds vehicle_charging_cmds energy_device_data energy_cmds"  # match with your application access request
+
+tesla_ble = True # True if commands to be sent via BLE
+tesla_remote = "user@192.168.1.77" # user:host if BLE command should be done on another device via SSH
+
